c3dca702cfb54ec0d82bcb0f7bd014dedae374f0
Alex Ho <81464025+hoyipyik@users.noreply.github.com>
2023-07-18 16:52:33 +0800

    add  peer remover

--- SLGSimulatorServer/ClientPeer.cs
+++ SLGSimulatorServer/ClientPeer.cs
@@ -17,6 +17,8 @@ namespace SLGSimulatorServer
         }
         protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
         {
+            // remove relevant peer from peer list
+            SLGSimulatorServer.PeerList.Remove(this);
             Utils.log.Info("Client disconnected. " + reasonCode + " " + reasonDetail);
         }
 
@@ -37,17 +39,30 @@ namespace SLGSimulatorServer
                     res = Service.AttackHandler(operationRequest.Parameters);
                     SendOperationResponse(res, sendParameters);
                     // await for 5 seconds to simulate the attack
-                    System.Threading.Thread.Sleep(5000);
+                    //System.Threading.Thread.Sleep(5000);
                     // send upgrade event to client
                     EventData eventData = Service.UpgradeTrigger();
-                    SendEvent(eventData, new SendParameters());
+                    //SendEvent(eventData, new SendParameters());
+                    SendEventToAll(eventData);
                     break;
                 case (byte)Service.MessageCode.upgradeInfo:
                     res = Service.UpgradeInfoHandler(operationRequest.Parameters);
                     SendOperationResponse(res, sendParameters);
+                    EventData newEventData = Service.UpgradeTrigger();
+                    //SendEvent(newEventData, new SendParameters());
+                    SendEventToAll(newEventData);
                     break;
                 default:
                     break;
+            }
+        }
+
+        private void SendEventToAll(EventData eventData)
+        {
+            Utils.log.Info("Send event to all "+ SLGSimulatorServer.PeerList.ToArray().Length);
+            foreach (var peer in SLGSimulatorServer.PeerList)
+            {
+                peer.SendEvent(eventData, new SendParameters());
             }
         }
     }
